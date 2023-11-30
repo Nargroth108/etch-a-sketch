c83b865832d29bd78302aa97b428b63d7d457d21
Nargroth108 <138591543+Nargroth108@users.noreply.github.com>
2023-11-30 12:35:43 +0100

    Make the grids appear squares using css grid

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 let container = document.querySelector('.container');
 
-for (i = 0; i < 256; i++){
+let numOf = prompt('choose a number');
+
+for (i = 0; i < numOf*numOf; i++){
     const div = document.createElement('div');
-    div.classList.add('div');
-    div.style.cssText = 'box-sizing: border-box; width: 20px; height: 20px; background-color: black;'
+    div.classList.add('square');
+    div.style.cssText = 'aspect-ratio: 1; background-color: black;'
     container.appendChild(div);
 };
 
